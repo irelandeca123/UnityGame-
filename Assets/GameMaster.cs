ca4af006cb97596033f52487407e072a1fd1d29c
Stoyan Rizv <stoyan.rizov6@gmail.com>
2016-04-02 17:21:35 +0100

    Contra Shooter

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour
+{
+
+    public static GameMaster gm;
+
+    private static int _remainingLives = 3;
+    public static int RemainingLives
+    {
+        get { return _remainingLives; }
+    }
+
+    void Awake()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public float spawnDelay = 2;
+   // public Transform spawnPrefab;
+
+ 
+    [SerializeField]
+    private GameObject gameOverUI;
+
+    void Start()
+    {
+      
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("GAME OVER");
+        gameOverUI.SetActive(true);
+    }
+
+    public IEnumerator _RespawnPlayer()
+    {
+     //   GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    //    GameObject clone = Instantiate(spawnPoint.position, spawnPoint.rotation) as GameObject;
+     //   Destroy(clone, 3f);
+    }
+
+    public static void KillPlayer(Player player)
+    {
+        Destroy(player.gameObject);
+        _remainingLives -= 1;
+        if (_remainingLives <= 0)
+        {
+            gm.EndGame();
+        }
+        else
+        {
+            gm.StartCoroutine(gm._RespawnPlayer());
+        }
+    }
+
+    public static void KillEnemy(Enemy enemy)
+    {
+        gm._KillEnemy(enemy);
+    }
+    public void _KillEnemy(Enemy _enemy)
+    {
+    
+        Destroy(_enemy.gameObject);
+    }
+
+}

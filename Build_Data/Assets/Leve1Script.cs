5d08cd4a6df64011c3b380adf2f954a34569482f
Stoyan Rizv <stoyan.rizov6@gmail.com>
2016-04-04 17:21:34 +0100

    update

---
+++ Build_Data/Assets/Leve1Script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Leve1Script : MonoBehaviour {
+
+    // Use this for initialization
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //if the object that triggered the event is tagged player
+        if (other.tag == "Player")
+        {
+            //Application.LoadLevel(1);
+            Application.LoadLevel(2);
+        }
+
+        //		if (other.gameObject.transform.parent) {
+        //			Destroy (other.gameObject.transform.parent.gameObject);
+        //			
+        //		} else {
+        //			Destroy (other.gameObject);		
+        //		}
+    }
+}
---
+++ Build_Data/Assets/Level2Script.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2Script : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //if the object that triggered the event is tagged player
+        if (other.tag == "Player")
+        {
+            //Application.LoadLevel(1);
+            Application.LoadLevel(3);
+        }
+
+        //		if (other.gameObject.transform.parent) {
+        //			Destroy (other.gameObject.transform.parent.gameObject);
+        //			
+        //		} else {
+        //			Destroy (other.gameObject);		
+        //		}
+    }
+
+}
+
+
